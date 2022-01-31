71dc9a3561c85608577a2c944e3f011ab6937ba8
manikandan <maniramkr2006@gmail.com>
2022-01-31 13:54:19 +0530

    publish react 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
     </div>
   );
 }
 
 export default App;
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome name = "Ramalingam vaarungal Sutha Sanmargam" 
+      img="https://1.bp.blogspot.com/-aKXqFgRSmEs/XERF6liZb8I/AAAAAAABW2I/ELs0O87C-9Qq3N3_S9R-epcyCbeFXuWfQCLcBGAs/s1600/G_L3_914.JPG"/>
+        <Welcome name = "vallalar samarasa sutha sanmarga sangathil kooduvom"
+        img="https://soonyata.home.xs4all.nl/Soruba%20samadhi/Ramalinga-small.jpg"/>
+      <Welcome name = "ManiVani vanangukirom Sathya Gnanagh Sabai,dharumasalai"
+      img="https://gumlet.assettype.com/swarajya%2F2020-08%2F195c5be1-a49a-4328-84b2-977b20917dfd%2Fvallalar2.jpg?auto=format%2Ccompress&w=1200"/> 
+      </div> 
+  );
+}
+
+export default App;
+
+function Welcome({name,img}){
+  
+  return (
+    <div>
+      <img src ={img} id="img" alt=""/>
+      <h1>Hello,{name}👌🌹</h1>
+    </div>
+  )
+}
