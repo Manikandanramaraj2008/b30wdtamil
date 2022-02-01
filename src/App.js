c7fbb638260c51e9d62392837904ae9ff3c3edd4
manikandan <maniramkr2006@gmail.com>
2022-02-01 10:39:29 +0530

    profile pic display

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 
 import './App.css';
 
-export function App() {
-  return (
-    <div className="App">
-      
-        
-    </div>
-  );
-}
+ export default function App(){
+ const users=[
+            {
+        profile:"https://1.bp.blogspot.com/-aKXqFgRSmEs/XERF6liZb8I/AAAAAAABW2I/ELs0O87C-9Qq3N3_S9R-epcyCbeFXuWfQCLcBGAs/s1600/G_L3_914.JPG",
+        name:"Ramalingam vaarungal Sutha Sanmargam"
+   },
+   {
+         profile:"https://soonyata.home.xs4all.nl/Soruba%20samadhi/Ramalinga-small.jpg",
+         name:"vallalar samarasa sutha sanmarga sangathil kooduvom"
+        },
+ 
+        {
+          profile:"https://gumlet.assettype.com/swarajya%2F2020-08%2F195c5be1-a49a-4328-84b2-977b20917dfd%2Fvallalar2.jpg?auto=format%2Ccompress&w=1200",
+          name: "ManiVani vanangukirom Sathya Gnanagh Sabai,dharumasalai"
+        }
 
-export default App;
+      ];
+return <div className='App'>
+  {users.map((usr)=>(<Welcome name={usr.name}profile={usr.profile}/>
+       ))}
+  
+        </div>
+ } 
 
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Welcome name = "Ramalingam vaarungal Sutha Sanmargam" 
-      img="https://1.bp.blogspot.com/-aKXqFgRSmEs/XERF6liZb8I/AAAAAAABW2I/ELs0O87C-9Qq3N3_S9R-epcyCbeFXuWfQCLcBGAs/s1600/G_L3_914.JPG"/>
-        <Welcome name = "vallalar samarasa sutha sanmarga sangathil kooduvom"
-        img="https://soonyata.home.xs4all.nl/Soruba%20samadhi/Ramalinga-small.jpg"/>
-      <Welcome name = "ManiVani vanangukirom Sathya Gnanagh Sabai,dharumasalai"
-      img="https://gumlet.assettype.com/swarajya%2F2020-08%2F195c5be1-a49a-4328-84b2-977b20917dfd%2Fvallalar2.jpg?auto=format%2Ccompress&w=1200"/> 
-      </div> 
-  );
-}
-
-export default App;
-
-function Welcome({name,img}){
+function Welcome({name,profile}){
   
   return (
-    <div>
-      <img src ={img} id="img" alt=""/>
+    <div className="Welcome">
+      <img className="user-profile-pic" src ={profile} alt ="profile-pic"/>
       <h1>Hello,{name}👌🌹</h1>
     </div>
-  )
-}
+  );
+  }
+
+// function counter(){
+// return(
+//   <div>
+//     <button>like</button>
+//   </div>
+// )
+
+// }
+
+
+
